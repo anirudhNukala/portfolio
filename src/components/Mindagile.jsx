1f32838b2540fa286550d35ac8a4ca9726b3ab91
Anirudh Nukala <anirudh.nukala@gmail.com>
2023-12-30 23:01:26 -0600

    added mindagile

--- src/components/Mindagile.jsx
+++ src/components/Mindagile.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 
 const Mindagile = () => {
-    return (
-        <div style={{ display: "flex", alignItems: "center" }}>
-            <img
-                src="/images/mindagile_logo.jpeg"
-                alt="Mindagile Logo"
-                style={{ width: "48px", height: "48px", borderRadius: "50%", marginRight: "10px" }}
-            />
-            <div>
-                <p style={{ margin: 0 }}>Software Development Intern</p>
-                <p style={{ fontSize: "0.8em", color: "#888", margin: 0 }}>
-                    Mindagile Consulting
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        //border: "1px solid #000",
+        //padding: "10px", 
+      }}
+    >
+      <img
+        src="/images/mindagile_logo.jpeg"
+        alt="Mindagile Logo"
+        style={{
+          width: "48px",
+          height: "48px",
+          borderRadius: "50%",
+          marginRight: "10px",
+        }}
+      />
+      <div>
+        <p style={{ margin: 0 }}>Software Development Intern</p>
+        <p style={{ fontSize: "0.8em", color: "#888", margin: 0 }}>
+          Mindagile Consulting
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Mindagile;
